2e11ccc4b84e8ebc1f88570a852fd63e442418cc
Ali Akkas <aliakkas006@gmail.com>
2022-08-10 10:20:36 +0600

    authentication-system-added

--- server/server.js
+++ server/server.js
@@ -1,10 +1,73 @@
-const express = require("express");
+const express = require('express');
+const bcrypt = require('bcryptjs');
+const connectDB = require('./db');
+const User = require('./models/User');
+
 const app = express();
+app.use(express.json());
 
-app.get("/", (_, res) => {
-  res.send("Attendance system application is running now!");
+app.get('/', (_, res) => {
+  res.send('Attendance system application is running now!');
 });
 
-app.listen(5000, () => {
-  console.log("Server is running on port 5000");
+app.use((err, _req, res) => {
+  console.log(err);
+  res.status(500).json({ message: 'Server Error Occured' });
 });
+
+app.post('/login', async (req, res, next) => {
+  const { email, password } = req.body;
+
+  try {
+    const user = await User.findOne({ email });
+    if (!user) {
+      return res.status(400).json({ message: 'Invalid Credential' });
+    }
+
+    const isMatch = await bcrypt.compare(password, user.password);
+    if (!isMatch) {
+      return res.status(400).json({ message: 'Invalid Credential' });
+    }
+
+    delete user._doc.password;
+
+    return res.status(200).json({ message: 'Login Successfully', user });
+  } catch (err) {
+    console.log(err);
+    next(err);
+  }
+});
+
+app.post('/register', async (req, res, next) => {
+  const { name, email, password } = req.body;
+
+  if (!name || !email || !password)
+    return res.status(400).json({ message: 'Invalid Data' });
+
+  try {
+    let user = await User.findOne({ email });
+    if (user) return res.status(400).json({ message: 'User already exist!' });
+
+    user = new User({ name, email, password });
+
+    const salt = await bcrypt.genSalt(10);
+    const hash = await bcrypt.hash(password, salt); 
+    user.password = hash;
+
+    await user.save();
+
+    return res.status(201).json({ message: 'User created successfully', user });
+
+  } catch (err) {
+    next(err);
+  }
+});
+
+connectDB('mongodb://localhost:27017/attendance-db')
+  .then(() => {
+    console.log('Database connected!');
+    app.listen(5000, () => {
+      console.log('Server is running on port 5000');
+    });
+  })
+  .catch((err) => console.log("database error", err));

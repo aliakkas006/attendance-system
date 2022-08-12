a7f995a7572f0afd1c6bf1db24c9f099d41e68c8
Ali Akkas <aliakkas006@gmail.com>
2022-08-12 20:22:36 +0600

    implement-user-crud-operations

---
+++ server/controller/users.js
@@ -0,0 +1,114 @@
+const User = require('../models/User');
+const userService = require('../service/user');
+const error = require('../utils/error');
+const authService = require('../service/auth');
+
+const getUsers = async (req, res, next) => {
+  /**
+   * TODO: filter, sort, pagination, select
+   */
+  try {
+    const users = await userService.findUsers();
+    res.status(200).json(users);
+  } catch (e) {
+    next(e);
+  }
+};
+
+const getUserById = (req, res, next) => {
+  const { userId } = req.params;
+
+  try {
+    const user = userService.findUserByProperty('_id', userId);
+
+    if (!user) throw error('User not found', 404);
+
+    res.status(200).json(user);
+  } catch (e) {
+    next();
+  }
+};
+
+const postUser = async (req, res, next) => {
+  const { name, email, password, roles, accountStatus } = req.body;
+
+  try {
+    const user = await authService.registerService({
+      name,
+      email,
+      password,
+      roles,
+      accountStatus,
+    });
+
+    res.status(201).json(user);
+  } catch (e) {
+    next(e);
+  }
+};
+
+const putUserById = (req, res, next) => {
+  const { userId } = req.params;
+  const { name, email, roles, accountStatus } = req.body;
+
+  try {
+    const user = userService.updateUser(userId, {
+      name,
+      email,
+      roles,
+      accountStatus,
+    });
+
+    if (!user) throw error('User not found', 404);
+
+    return res.status(200).json(user);
+  } catch (e) {
+    next();
+  }
+};
+
+const patchUserById = async (req, res, next) => {
+  const { userId } = req.params;
+  const { name, roles, accountStatus } = req.body;
+
+  try {
+    const user = await userService.findUserByProperty('_id', userId);
+
+    if (!user) throw error('User not found', 404);
+
+    user.name = name ?? user.name;
+    user.roles = roles ?? user.roles;
+    user.accountStatus = accountStatus ?? user.accountStatus;
+
+    await user.save();
+
+    res.status(200).json(user);
+  } catch (e) {
+    next(e);
+  }
+};
+
+const deleteUserById = async (req, res, next) => {
+  const { userId } = req.params;
+
+  try {
+    const user = await userService.findUserByProperty('_id', userId);
+
+    if (!user) throw error('User not found', 404);
+
+    await user.remove();
+
+    return res.status(203).send();
+  } catch (e) {
+    next(e);
+  }
+};
+
+module.exports = {
+  getUsers,
+  getUserById,
+  postUser,
+  putUserById,
+  patchUserById,
+  deleteUserById,
+};
---
+++ server/routes/users.js
@@ -0,0 +1,46 @@
+const router = require('express').Router();
+const userController = require('../controller/users');
+
+/**
+ * get user by id
+ * @method GET
+ */
+router.get('/:userId', userController.getUserById);
+
+/**
+ * update user by id
+ * @method PUT
+ */
+router.put('/:userId', userController.putUserById);
+
+/**
+ * update user by id
+ * @method PATCH
+ */
+router.patch('/:userId', userController.patchUserById);
+
+/**
+ * delete user by id
+ * @method DELETE
+ */
+router.delete('/:userId', userController.deleteUserById);
+
+/**
+ * Get all users include:
+ * filter
+ * sort
+ * pagination
+ * select properties
+ * @route /api/v1/users?sort=["by","name"]
+ * @method GET
+ * @visibility private
+ */
+router.get('/', userController.getUsers);
+
+/**
+ * create a new user
+ * @method POST
+ */
+router.post('/', userController.postUser);
+
+module.exports = router;
